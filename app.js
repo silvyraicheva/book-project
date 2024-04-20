0f717756a77cdc9699ec9f51f2f41fbb0a0b2bc0
Silvy Raicheva <sylvie.raicheva@gmail.com>
2024-04-20 18:15:56 +0300

    test websocket

--- app.js
+++ app.js
@@ -3,11 +3,15 @@ var express = require("express");
 var path = require("path");
 var cookieParser = require("cookie-parser");
 var logger = require("morgan");
+const socketIo = require("socket.io");
+//
 
+//
 var indexRouter = require("./routes/index");
 var usersRouter = require("./routes/users");
 var booksRouter = require("./routes/books");
 var app = express();
+// Initialize WebSocket connection
 
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
